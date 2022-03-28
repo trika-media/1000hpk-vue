b4bbbee9f25777e6cbacc9eb1e6db5944b3272fa
abela-a <abelardhana96@gmail.com>
2022-03-28 22:27:24 +0800

    [base] pagination

---
+++ resources/js/Components/Datatable/Pagination.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from '@vue/runtime-core';
+import { Link } from "@inertiajs/inertia-vue3";
+
+const props = defineProps({
+    links: Array,
+    total: Number,
+    from: Number,
+    to: Number,
+});
+
+const links = computed(() => props.links);
+</script>
+
+<template>
+    <div v-if="links.length > 3" class="border-0 d-flex flex-column flex-lg-row align-items-center justify-content-between">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination mb-0">
+                <li class="page-item" v-for="(link, p) in links" :key="p" :class="{ 'active': link.active, 'disabled': link.url === null }">
+                    <Link class="page-link" :href="link.url">
+                        {{ link.label }}
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="fw-normal small mt-4 mt-lg-0">
+            Menampilkan {{ props.from }} sampai {{ props.to }} dari <b>{{ props.total }}</b> data.
+        </div>
+    </div>
+</template>
