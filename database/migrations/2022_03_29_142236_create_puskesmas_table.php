dde5bf53f7b35017bf59e90c6505e2c3dda23bfd
abela-a <abelardhana96@gmail.com>
2022-03-29 14:23:37 +0800

    [master] perbaiki migrasi penyakit

--- database/migrations/2022_03_29_142236_create_puskesmas_table.php
+++ database/migrations/2022_03_29_142236_create_puskesmas_table.php
@@ -13,13 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('penyakit', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-
-            $table->string('kode')->unique();
-            $table->string('nama');
-            $table->string('kelompok')->nullable();
-
+        Schema::create('puskesmas', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -31,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('penyakits');
+        Schema::dropIfExists('puskesmas');
     }
 };
