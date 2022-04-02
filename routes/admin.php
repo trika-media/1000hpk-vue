6deb3dd5a9ad2e0de7268a84035fbecc5acb1305
abela-a <abelardhana96@gmail.com>
2022-04-02 09:08:53 +0800

    memisahkan route admin dan user

---
+++ routes/admin.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\IbuHamilController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PenggunaController;
+use App\Http\Controllers\PenyakitController;
+use App\Http\Controllers\PuskesmasController;
+use Illuminate\Support\Facades\Route;
+
+# KUMPULAN ROUTE YANG DIGUNAKAN UNTUK ROLE ADMIN/SUPERADMIN
+# MIDDLEWARE = roles:superadmin,admin
+
+Route::middleware('roles:superadmin,admin')->group(function () {
+    # BERITA
+    Route::resource('berita', BeritaController::class)
+        ->parameters(['berita' => 'berita']);
+    Route::post('berita/{berita}/status', [BeritaController::class, 'status'])
+        ->name('berita.status');
+
+    # MAHASISWA
+    Route::resource('mahasiswa', MahasiswaController::class);
+
+    Route::prefix('master')->name('master.')->group(function () {
+        # PENGGUNA
+        Route::resource('pengguna', PenggunaController::class);
+        Route::post('pengguna/{pengguna}/status', [PenggunaController::class, 'status'])
+            ->name('pengguna.status');
+
+        # PENYAKIT
+        Route::resource('penyakit', PenyakitController::class);
+
+        # PUSKESMAS
+        Route::resource('puskesmas', PuskesmasController::class)
+            ->parameters(['puskesmas' => 'puskesmas']);
+    });
+});
---
+++ routes/user.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\User\DataDiriController;
+use App\Http\Controllers\User\ListBeritaController;
+use Illuminate\Support\Facades\Route;
+
+# KUMPULAN ROUTE YANG DIGUNAKAN UNTUK ROLE MAHASISWA DAN USER LAINNYA
+# MIDDLEWARE = roles:user,mahasiswa || mahasiswa
+
+Route::prefix('user')->name('user.')->middleware('roles:user,mahasiswa')->group(function () {
+    # BERITA, Hanya list untuk mahasiswa
+    Route::resource('berita', ListBeritaController::class)
+        ->parameters(['berita' => 'slug'])
+        ->only('index', 'show');
+
+    Route::middleware('mahasiswa')->group(function () {
+        # DATA DIRI MAHASISWA
+        Route::resource('data-diri', DataDiriController::class)
+            ->only('index', 'store');
+    });
+});
